c8b2a53ec4ef5030b272c49749332fb3d10c4859
pgarman524 <151721693+pgarman524@users.noreply.github.com>
2024-02-12 11:21:54 +0100

    Corrección de  validadores

--- act09/concesionario.py
+++ act09/concesionario.py
@@ -4,55 +4,57 @@ from jsonschema import validate
 # Definir el esquema
 schema = {
     "$schema": "http://json-schema.org/draft-07/schema#",
-
     "type": "object",
     "properties": {
-      "Concesionario": {
-        "type": "object",
-        "properties": {
-          "concesionario": {
+        "Concesionario": {
             "type": "object",
             "properties": {
-              "cod_coche": {
-                "type": "number"
-              },
-              "marca": {
-                "type": "string"
-              },
-              "modelo": {
-                "type": "string"
-              },
-              "matricula": {
-                "type": "number"
-              },
-              "potencia": {
-                "type": "string"
-              },
-              "plazas": {
-                "type": "number"
-              },
-              "num_puertas": {
-                "type": "number"
-              }
+                "concesionario": {
+                    "type": "object",
+                    "properties": {
+                        "cod_coche": {
+                            "type": "number",
+                            "minimum":  100000000,
+                            "maximum":  999999999
+                        },
+                        "marca": {
+                            "type": "string"
+                        },
+                        "modelo": {
+                            "type": "string"
+                        },
+                        "matricula": {
+                            "type": "string",
+                            "pattern": "^[0-9]{8}[a-zA-Z]$"
+                        },
+                        "potencia": {
+                            "type": "string"
+                        },
+                        "plazas": {
+                            "type": "number"
+                        },
+                        "num_puertas": {
+                            "type": "number"
+                        }
+                    },
+                    "required": [
+                        "cod_coche",
+                        "marca",
+                        "modelo",
+                        "matricula",
+                        "potencia",
+                        "plazas",
+                        "num_puertas"
+                    ]
+                }
             },
             "required": [
-              "cod_coche",
-              "marca",
-              "modelo",
-              "matricula",
-              "potencia",
-              "plazas",
-              "num_puertas"
+                "concesionario"
             ]
-          }
-        },
-        "required": [
-          "concesionario"
-        ]
-      }
+        }
     },
     "required": [
-      "Concesionario"
+        "Concesionario"
     ]
 }
 
@@ -60,17 +62,17 @@ schema = {
 archivo_json = '''
 {
     "Concesionario": {
-      "concesionario": {
-        "cod_coche": 2511600956,
-        "marca": "CITROEN",
-        "modelo": "C3 Aircross PureTech",
-        "matricula": 567489120,
-        "potencia": "110CV",
-        "plazas": 5,
-        "num_puertas": 4
-      }
+        "concesionario": {
+            "cod_coche":  123456789,
+            "marca": "CITROEN",
+            "modelo": "C3 Aircross PureTech",
+            "matricula": "56748912A",
+            "potencia": "110CV",
+            "plazas":  5,
+            "num_puertas":  4
+        }
     }
-  }
+}
 '''
 
 # Cargar el archivo JSON
@@ -79,6 +81,6 @@ datos_json = json.loads(archivo_json)
 # Validar contra el esquema
 validate(instance=datos_json, schema=schema)
 
-#Este script de Python utiliza la biblioteca jsonschema para cargar el esquema y los datos JSON, 
-#y luego realiza la validación. Si el archivo JSON cumple con el esquema, no se producirá ninguna excepción. 
-#En caso contrario, se lanzará una excepción indicando la razón de la invalidación.
+# Este script de Python utiliza la biblioteca jsonschema para cargar el esquema y los datos JSON,  
+# y luego realiza la validación. Si el archivo JSON cumple con el esquema, no se producirá ninguna excepción.  
+# En caso contrario, se lanzará una excepción indicando la razón de la invalidación.
